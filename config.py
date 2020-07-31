fab388e62183c395e8703434828eaf1d3d380dcd
ashirafumiiro <ashirafumiiro@gmail.com>
2020-07-31 17:12:54 +0300

    added databases

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changedma'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
